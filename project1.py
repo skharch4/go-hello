cdba9040fdd9504b4498e7a720ac8ced7b98ebc2
skharch4 <skharch4@gmail.com>
2022-08-01 11:57:36 +0300

    Project

---
+++ project1.py
@@ -0,0 +1,44 @@
+print("Hello,Everybody")
+if 26>25:
+    print ("Twenty six is bigger than twenty five")
+    #Is it correct?
+    #Is it wrong?
+    x = 21
+    y = "Steve"
+    print(x)
+    print(y)
+    x = str(21) # x will be '21'
+    y = int(21) # y will be 21
+    z = float (21) # z will be 21.0
+    print(x)
+    print(y)
+    print(z)
+    print (type(x))
+    print (type (y))
+    print(type(z))
+    b = 4
+    B = 5
+    
+    print(b)
+    print(B)
+    myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+myvar = "Jimmy"
+print(myvar)
+    
+
+
+    
+    
