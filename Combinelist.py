617d9f42106494024acaa74a37910bfffefd96d3
skharch4 <skharch4@gmail.com>
2022-09-13 22:01:27 +0300

    Print different fruits of different colors

---
+++ Combinelist.py
@@ -0,0 +1,9 @@
+list1 =["Red","Green","Yellow"]
+list2=["Banana","Melon","Apple"]
+i = 0
+while i < len(list1):
+    j = 0
+    while j < len(list2):
+        print(list1[i], " ",list2[j])
+        j=j+1
+    i=i+1
